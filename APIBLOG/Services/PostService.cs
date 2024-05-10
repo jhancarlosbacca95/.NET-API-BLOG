631d0e320d03b0d5b7b94f89c7ffc1fb80d6c141
Jhan Carlos <carlosbacca.1995@gmail.com>
2024-05-10 15:00:52 -0500

    Se crea el servicio para post, a demas de la interfaz del mismo

--- APIBLOG/Services/PostService.cs
+++ APIBLOG/Services/PostService.cs
@@ -50,11 +50,51 @@ namespace APIBLOG.Services
             }
         }
 
-        public async Task<bool> Save(Post post) 
+        public async Task<ICollection<Post>>GetByCategoria(List<int> idsCat)
+        {
+            try
+            {
+                //buscar las categorias por sus ids
+                var categorias = await _context.Categorias.Where(c => idsCat.Contains(c.IdCategoria)).ToListAsync();
+
+                //obtener los ids de los posts asociados a las categorias
+                var postsIds = categorias.SelectMany(c => c.IdPosts.Select(p=>p.IdPost)).Distinct();
+
+
+                //obtener los posts con dichas ids
+                var posts = await _context.Posts
+                    .Where(p => postsIds.Contains(p.IdPost))
+                    .ToListAsync();
+
+                return posts;
+            }
+            catch (DbException ex)
+            {
+                throw new Exception($"Error en la base de datos, Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al modificar el post", ex);
+            }
+        }
+        public async Task<bool> Save(Post post,List<int> CategoriasIds) 
         {
             try
             {
                 _context.Add(post);
+
+                if (CategoriasIds != null && CategoriasIds.Any())
+                {
+                    foreach (var categoriaId in CategoriasIds)
+                    {
+                        var categoria = await _context.Categorias.FindAsync(categoriaId);
+                        if (categoria != null) 
+                        {
+                            post.IdCategoria.Add(categoria);
+                            categoria.IdPosts.Add(post);
+                        }
+                    }
+                }
                 await _context.SaveChangesAsync();
                 return true;
             }
@@ -67,26 +107,52 @@ namespace APIBLOG.Services
             }
         }
 
-        public async Task<bool> Update(int id,Post post) 
+        public async Task<bool> Update(int id,Post post,List<int> CategoriasIds) 
         {
             try
             {
+                //buscar el post a modificar
                 var postActual = await _context.Posts.FindAsync(id);
                 if (postActual == null)
                     return false;
                 else
                 {
+                    foreach (var categoria in postActual.IdCategoria.ToList())
+                    {
+                        //elimina tanto los posts de la coleccion de categorias,
+                        //como la categoria de la coleccion de posts
+                        categoria.IdPosts.Remove(postActual);
+                        postActual.IdCategoria.Remove(categoria);
+                    }
+
+                    //modificar los atributos del post
                     postActual.Titulo = post.Titulo ?? postActual.Titulo;
                     postActual.Activo = post.Activo ?? postActual.Activo;
                     postActual.Contenido = post.Contenido ?? postActual.Contenido;
                     postActual.Imagen = post.Imagen ?? postActual.Imagen;
                     postActual.FechaActualizacion = DateTime.UtcNow;
+
+                    //Actualizar las colecciones de categoria y de posts.
+                    foreach (var categoriaId in CategoriasIds)
+                    {
+                        //buscar y asignar las categorias recibidas al post
+                        var categoria = await _context.Categorias.FindAsync(categoriaId);
+                        if (categoria!=null)
+                        {
+                            
+                            postActual.IdCategoria.Add(categoria);
+                            categoria.IdPosts.Add(postActual);
+                        }
+                    }
+
+                    //guardas los cambios en la base de datos 
                     _context.Posts.Update(postActual);
                     await _context.SaveChangesAsync();
 
                     return true;
                 }
             }
+            //captura de excepciones a nivel de base de datos y a nivel de servidor
             catch (DbException ex)
             {
                 throw new Exception($"Error en la base de datos, Error: {ex.Message}");
@@ -97,10 +163,12 @@ namespace APIBLOG.Services
             }
         }
 
+        //Eliminar post
         public async Task<bool>Delete(int id)
         {
             try
             {
+                //buscar post a eliminar
                 var postAEliminar = await _context.Posts.FindAsync(id);
 
                 if (postAEliminar==null)
@@ -109,11 +177,19 @@ namespace APIBLOG.Services
                 }
                 else
                 {
+                    // Eliminar el post de la colección de categorías asociadas
+                    foreach (var categoria in postAEliminar.IdCategoria)
+                    {
+                        categoria.IdPosts.Remove(postAEliminar);
+                    }
+
+                    //Eliminar y guardar cambios en caso de encontrar el post
                     _context.Posts.Remove(postAEliminar);
                     await _context.SaveChangesAsync();
                     return true;
                 }
             }
+            //captura de excepciones a nivel de base de datos y a nivel de servidor
             catch (DbException ex)
             {
                 throw new Exception($"Error en la base de datos, Error: {ex.Message}");
--- APIBLOG/obj/Debug/net8.0/APIBLOG.AssemblyInfo.cs
+++ APIBLOG/obj/Debug/net8.0/APIBLOG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIBLOG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d6425406b2f4cf3e78ae5c226b2cfcab5dcb48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61a9b7cc2b4d77677cab30eefaa63d50f6d8335")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIBLOG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIBLOG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
