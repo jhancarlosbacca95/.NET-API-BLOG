5ecb149c49e6be788623d7189497e43b1691727a
Jhan Carlos <carlosbacca.1995@gmail.com>
2024-05-14 12:47:00 -0500

    commit para terminar los endpoints del controlador de posts

---
+++ APIBLOG/Controllers/PostController.cs
@@ -0,0 +1,154 @@
+﻿using APIBLOG.Models;
+using APIBLOG.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace APIBLOG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerPosts()
+        {
+            try
+            {
+                var posts = await _postService.Get();
+                if (posts == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(new { Message = "Ok", Response = posts });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en el servidor", ex);
+            }
+        }
+
+        [HttpGet("{idPost}")]
+        public async Task<IActionResult> ObtenerPorId(int idPost)
+        {
+            try
+            {
+                var post = await _postService.GetById(idPost);
+                if (post == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(new { Message = "Ok", Response = post });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en el servidor", ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerPorId([FromQuery] List<int> idCategorias)
+        {
+            try
+            {
+                var posts = await _postService.GetByCategoria(idCategorias);
+                if (posts == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(new { Message = "Ok", Response = posts });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en el servidor", ex);
+            }
+        }
+
+        public class PostConCategoriasDTO
+        {
+            public Post NuevoPost { get; set; }
+            public List<int> CategoriasIds { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GuardarPost([FromBody] PostConCategoriasDTO postYCategoria)
+        {
+            try
+            {
+                var resultado = await _postService.Save(postYCategoria.NuevoPost, postYCategoria.CategoriasIds);
+                if (!resultado)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok("Post guardado correctamente");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en el servidor", ex);
+            }
+        }
+
+        [HttpPut("{idPost}")]
+        public async Task<IActionResult> Modificar([FromBody] PostConCategoriasDTO postConCategoriasDTO, int idPost)
+        {
+            try
+            {
+                var resultado = await _postService.Update(idPost, postConCategoriasDTO.NuevoPost, postConCategoriasDTO.CategoriasIds);
+                if (!resultado)
+                {
+                    return NotFound("No se encontro el Post a eliminar");
+                }
+                else
+                {
+                    return Ok("Post modificado correctamente");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en el servidor", ex);
+            }
+        }
+
+        [HttpDelete("{idPost}")]
+        public async Task<IActionResult> EliminarPost(int idPost)
+        {
+            try
+            {
+                var resultado = await _postService.Delete(idPost);
+                if (!resultado)
+                {
+                    return NotFound("No se encontro el post a eliminar");
+                }
+                else
+                {
+                    return Ok("Post eliminado");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en el servidor", ex);
+            }
+        }
+
+
+    }
+}
---
+++ APIBLOG/Services/Interfaces/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using APIBLOG.Models;
+
+namespace APIBLOG.Services.Interfaces
+{
+    public interface IPostService
+    {
+        public Task<ICollection<Post>> Get();
+        public Task<Post> GetById(int id);
+        public Task<ICollection<Post>> GetByCategoria(List<int> idsCat);
+        public Task<bool> Save(Post post, List<int> CategoriasIds);
+        public Task<bool> Update(int id, Post post, List<int> CategoriasIds);
+        public Task<bool> Delete(int id);
+    }
+}
